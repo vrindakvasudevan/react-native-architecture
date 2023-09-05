0a1abf6039c97d53184ac3a013b73ebbbc163e3a
Vrinda Koorikandathil <vkoorikandathil@trl.co.uk>
2023-09-05 13:49:07 +0530

    Prettier Added

--- reactNativeArchitecture/.eslintrc.js
+++ reactNativeArchitecture/.eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
   'extends': [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/react'
   ],
   'overrides': [
   ],
