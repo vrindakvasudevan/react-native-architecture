9eed3ac277138606e883b52d4c07f1fb73a40879
Vrinda Koorikandathil <vkoorikandathil@trl.co.uk>
2023-09-05 13:47:25 +0530

    ESLint Added

--- reactNativeArchitecture/.eslintrc.js
+++ reactNativeArchitecture/.eslintrc.js
@@ -1,4 +1,41 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    'react-native',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
 };
