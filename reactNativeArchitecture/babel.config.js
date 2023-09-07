62772875fd39a414576bf0d2d0714841bd0a1293
Vrinda Koorikandathil <vkoorikandathil@trl.co.uk>
2023-09-07 09:27:17 +0530

    Relative path added

--- reactNativeArchitecture/babel.config.js
+++ reactNativeArchitecture/babel.config.js
@@ -1,3 +1,34 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.ios.jsx',
+          '.android.jsx',
+          '.js',
+          '.jsx',
+          '.json',
+          '.ts',
+          '.tsx',
+        ],
+        root: ['.'],
+        alias: {
+          '@api': './src/api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@i18n': './src/i18n',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+          '@env': './src/env.js',
+        },
+      },
+    ],
+  ],
 };
