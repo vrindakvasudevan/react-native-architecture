3cb017f3633eb05ccafb2a638fd1814158389757
vrindakvasudevan <vrinda.k@experionglobal.com>
2023-09-15 12:36:50 +0530

    Updated rule for linebreak-style

--- reactNativeArchitecture/.eslintrc.js
+++ reactNativeArchitecture/.eslintrc.js
@@ -1,42 +1,31 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
+  extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
   ],
-  'overrides': [
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'plugins': [
-    'react',
-    'react-native',
-    '@typescript-eslint'
-  ],
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ]
+  plugins: ['react', 'react-native', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
   },
-  ignorePatterns: ['.eslintrc.js','babel.config.js','metro.config.js','crypto-custom.js','jest.config.js']
+  ignorePatterns: [
+    '.eslintrc.js',
+    'babel.config.js',
+    'metro.config.js',
+    'crypto-custom.js',
+    'jest.config.js',
+  ],
 };
